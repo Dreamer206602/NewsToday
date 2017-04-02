9fc7f87e30f0d24334277ce1af5e0c6ae8809b85
Dreamer206602 <boobooMX@163.com>
2017-04-02 18:23:57 +0800

    Initial commit

---
+++ app/src/main/java/com/booboomx/todaynews/app/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.booboomx.todaynews.app;
+
+import android.app.Application;
+
+/**
+ * Created by booboomx on 17/4/2.
+ */
+
+public class BaseApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
